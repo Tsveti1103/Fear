40ebd108d07d7c1bcf6eb5211dc5e95fbc4f6f12
Tsveti1103 <cvstoqnova@gmail.com>
2023-04-25 16:45:54 +0300

    make all pages responsive

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,46 +2,54 @@ import styles from './Header.module.css';
 import simpleStyles from '../commonStyles/simpleButton.module.css';
 import { Link } from 'react-router-dom';
 import { useAuthContext } from "../../contexts/AuthContext";
+import { useState } from 'react';
 
 export default function Navigation() {
 	const { user } = useAuthContext();
+	const [showNav, setShowNav] = useState(true)
+	function showNavbar() {
+		setShowNav(!showNav)
+	}
+	function onLinkClick() {
+		setShowNav(true)
+	}
 	return (
 		<header className={styles.header}>
-			<nav className={styles.navbar}>
 
+			<nav className={styles.navbar}>
 				<div className={styles.logo}>
-					<Link to="/">FEAR</Link>
+					<Link to="/" onClick={onLinkClick}>FEAR</Link>
 				</div>
-				<ul role="list" className={styles.links}>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/fears">Catalog</Link></li>
-					<li><Link to="/about">About</Link></li>
-					<li><Link to="/contactus">Contact Us</Link></li>
-					<li><Link to="/map">Fears Map</Link></li>
+				<ul role="list" className={`${styles.links} ${showNav ? styles.active : ""}`}>
+					<li><Link to="/" onClick={onLinkClick}>Home</Link></li>
+					<li><Link to="/fears" onClick={onLinkClick}>Catalog</Link></li>
+					<li><Link to="/about" onClick={onLinkClick}>About</Link></li>
+					<li><Link to="/contactus" onClick={onLinkClick}>Contact Us</Link></li>
+					<li><Link to="/map" onClick={onLinkClick}>Fears Map</Link></li>
 					{user &&
-						<li><Link to="/create">Create Fear</Link></li>
+						<li><Link to="/create" onClick={onLinkClick}>Create Fear</Link></li>
 					}
 					{user &&
-						<li><Link to="/profile">Profile</Link></li>
+						<li><Link to="/profile" onClick={onLinkClick}>Profile</Link></li>
+					}
+					{user ?
+						<div className={styles.btns}>
+							<Link className={simpleStyles.simple} onClick={onLinkClick} to="/logout">Logout</Link>
+						</div>
+						:
+						<div className={styles.btns}>
+							<Link className={simpleStyles.simple} onClick={onLinkClick} to="/login">Login</Link>
+							<Link className={simpleStyles.simple} onClick={onLinkClick} to="/register">Register</Link>
+						</div>
 					}
 				</ul>
-				{user ?
-					<div className={styles.right}>
-						<Link className={simpleStyles.simple} to="/logout">Logout</Link>
-					</div>
-					:
-					<div className={styles.right}>
-						<Link className={simpleStyles.simple} to="/login">Login</Link>
-						<Link className={simpleStyles.simple} to="/register">Register</Link>
-					</div>
-
-				}
-
-				<div className={styles.toggle}>
-					<div className={styles.line1}></div>
-					<div className={styles.line2}></div>
-					<div className={styles.line3}></div>
-				</div>
+				<button onClick={showNavbar} className={styles.toggle}>
+					{showNav ?
+						<i className="fa-solid fa-bars" ></i>
+						:
+						<i class="fa-solid fa-xmark"></i>
+					}
+				</button>
 			</nav>
 		</header>
 	);
